1526867a2ce936349889c132014f850dc0ae0931
Aletha Wheeless <augustkama98@gmail.com>
2022-04-25 16:48:03 -1000

    commit tree 1667a9c6a4ee814232174c8a805206c7f4d1bda8

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+
+function saturdayFun(activity) {
+    if (activity === undefined) {
+        activity = 'roller-skate';
+    }
+   let fun = `This Saturday, I want to ${activity}!`
+    return fun;
+}
+function mondayWork(activity) {
+    if (activity === undefined) {
+        activity = 'go to the office';
+    }
+    let monday = `This Monday, I will ${activity}.`
+    return monday;
+}
+function wrapAdjective(special = '*',) {
+    const innerFunction = function (adjective = 'a hard worker') {
+      return `You are ${special}${adjective}${special}!`;
+    };
+    return innerFunction;
+  }
